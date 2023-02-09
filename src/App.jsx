827a9ca01132c5bd81373019ce29c5741a47d8e5
essie20 <ryghl683@gmail.com>
2023-02-09 21:40:22 +0200

    styling, install react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
+import { useQueryClient } from 'react-query'
 
 function App() {
+  const queryClient = useQueryClient()
   const [passengers, setPassengers] = useState(1);
   const [origin, setOrigin] = useState("");
   const [destination, setDestination] = useState("");
@@ -12,47 +14,59 @@ function App() {
     console.log(origin)
     console.log(destination)
     console.log(departureDate)
+    // TODO send API Call using react query
   }
 
   return (
     <div className="App">
-      <h1>Flight Status</h1>
-      <div className="grid grid-cols-3">
-        
-        <div>
-          <label>From</label>
-          <input
-            className="border-solid border-2 border-indigo-500 rounded" 
-            type="text"
-            value={origin}
-            onChange={(e) => setOrigin(e.target.value)} 
-          />
+      <h1 className="text-3xl font-bold mb-5">Flight Status</h1>
+      <div className="flex flex-row gap-4 mb-4 bg-blue-900 p-5 rounded-2xl">
+        <div className="border-solid border border-blue-300 rounded bg-white basis-1/4">
+          <label>
+            <span className="mr-2 block pt-3 pl-5 text-gray-500">From</span>
+            <input
+              className="w-full outline-0 px-5 pb-2 text-xl"
+              type="text"
+              value={origin}
+              onChange={(e) => setOrigin(e.target.value)}/>
+          </label>
         </div>
-        <div>
-          <label>To</label>
-          <input
-            className="border-solid border-2 border-indigo-500 rounded" 
-            type="text"
-            value={destination}
-            onChange={(e) => setDestination(e.target.value)} 
-          />
+        <div className="border-solid border border-blue-300 rounded bg-white basis-1/4">
+          <label>
+            <span className="mr-2 block pt-3 pl-5 text-gray-500">To</span>
+            <input
+              className="w-full outline-0 px-5 pb-2 text-xl"
+              type="text"
+              value={destination}
+              onChange={(e) => setDestination(e.target.value)}/>
+          </label>
         </div>
-        <div>
-          <label>Passengers</label>
-          <input
-            className="border-solid border-2 border-indigo-500 rounded" 
-            type="number" 
-            value={passengers}
-            onChange={(e) => setPassengers(e.target.value)}/>
+        <div className="border-solid border border-blue-300 rounded bg-white basis-1/4">
+          <label>
+            <span className="mr-2 block pt-3 pl-5 text-gray-500">Departure Date</span>
+            <input
+              className="w-full outline-0 px-5 pb-2 text-xl"
+              type="date" 
+              value={departureDate}
+              onChange={(e) => setDepartureDate(e.target.value)}/>
+          </label>
         </div>
+        <div className="border-solid border border-blue-300 rounded bg-white basis-1/6">
+          <label>
+            <span className="mr-2 block pt-3 pl-5 text-gray-500">Passengers</span>
+            <input
+              className="w-full outline-0 px-5 pb-2 text-xl"
+              type="number" 
+              value={passengers}
+              onChange={(e) => setPassengers(e.target.value)}/>
+          </label>
+        </div>
+        <button 
+          className="border-solid border text-white border-green-700 bg-green-500 rounded-full px-4 py-2 ml-2 mt-2 font-semibold w-32 h-14" 
+          onClick={() => performSearch()}>
+          Search
+        </button>
       </div>
-      <label>Departure Date</label>
-      <input
-        className="border-solid border-2 border-indigo-500 rounded" 
-        type="date" 
-        value={departureDate}
-        onChange={(e) => setDepartureDate(e.target.value)}/>
-      <button onClick={() => performSearch()}>Search</button>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './main.css'
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
