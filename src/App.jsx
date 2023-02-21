d5e5ff1255e9527eae57e18a6d07ddc72fc2e4e2
essie20 <ryghl683@gmail.com>
2023-02-21 02:07:51 +0200

    install prettier, styling improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,83 @@
-import { useState } from 'react'
-import { useQueryClient, useQuery } from '@tanstack/react-query'
-import { getDepartures } from './api/flight';
-import { format, addDays } from 'date-fns'
-import Input from './components/Input';
-import FlightList from './components/FlightList';
+import { useState } from "react";
+import { useQueryClient, useQuery } from "@tanstack/react-query";
+import { getDepartures } from "./api/flight";
+import { format, addDays } from "date-fns";
+import Input from "./components/Input";
+import FlightList from "./components/FlightList";
 
 function App() {
-  const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
   const [passengers, setPassengers] = useState(1);
   const [origin, setOrigin] = useState("");
   const [destination, setDestination] = useState("");
-  const minmumDate = format(addDays(new Date(), 8), 'yyyy-MM-dd')
+  const minmumDate = format(addDays(new Date(), 8), "yyyy-MM-dd");
   const [departureDate, setDepartureDate] = useState(minmumDate);
 
-  const { loading, error, data, refetch } = useQuery(["getDepartures"], () => {
-    const formattedDepartureDate = format(new Date(departureDate), 'yyyy-MM-dd')
-    return getDepartures(origin, destination, formattedDepartureDate, passengers) 
-  }, {
-    enabled: false
-  });
+  const { loading, error, data, refetch } = useQuery(
+    ["getDepartures"],
+    () => {
+      const formattedDepartureDate = format(
+        new Date(departureDate),
+        "yyyy-MM-dd"
+      );
+      return getDepartures(
+        origin,
+        destination,
+        formattedDepartureDate,
+        passengers
+      );
+    },
+    {
+      enabled: false,
+    }
+  );
 
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold mb-5">Flight Status</h1>
-      <div className="flex flex-row gap-4 mb-4 bg-blue-900 p-5 rounded-2xl">
-          <Input
-            className="basis-1/4"
-            label="From"
-            type="text"
-            value={origin}
-            onChange={setOrigin}
-          />
-          <Input
-            className="basis-1/4"
-            label="To"
-            type="text"
-            value={destination}
-            onChange={setDestination}
-          />
-          <Input
-            className="basis-1/4"
-            label="Departure Date"
-            type="date"
-            value={departureDate}
-            onChange={setDepartureDate} 
-            min={minmumDate}
-          />
-          <Input
-            className="basis-1/6"
-            label="Passengers"
-            type="number"
-            value={passengers} 
-            onChange={setPassengers}
-          />
-        <button 
-          className="border-solid border text-white border-green-700 bg-green-500 rounded-full px-4 py-2 ml-2 mt-2 font-semibold w-32 h-14" 
-          onClick={() => refetch()}>
+      <h1 className="mb-5 text-3xl font-bold">Flight Status</h1>
+      <div className="mb-4 flex flex-row gap-4 rounded-2xl bg-blue-900 p-5">
+        <Input
+          className="basis-1/4"
+          label="From"
+          type="text"
+          value={origin}
+          onChange={setOrigin}
+        />
+        <Input
+          className="basis-1/4"
+          label="To"
+          type="text"
+          value={destination}
+          onChange={setDestination}
+        />
+        <Input
+          className="basis-1/4"
+          label="Departure Date"
+          type="date"
+          value={departureDate}
+          onChange={setDepartureDate}
+          min={minmumDate}
+        />
+        <Input
+          className="basis-1/6"
+          label="Passengers"
+          type="number"
+          value={passengers}
+          onChange={(value) => {
+            value ? setPassengers(value) : setPassengers(1);
+          }}
+          min={1}
+        />
+        <button
+          className="ml-2 mt-2 h-14 w-32 rounded-full border border-solid border-green-700 bg-green-500 px-4 py-2 font-semibold text-white"
+          onClick={() => refetch()}
+        >
           Search
         </button>
       </div>
       {data ? <FlightList data={data} /> : "no data"}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/FlightList.jsx
+++ src/components/FlightList.jsx
@@ -1,61 +1,82 @@
-import React from 'react';
+import React from "react";
 
-export default function FlightList({ data, passengers }) {
+export default function FlightList({ data }) {
   return (
     <div className="pt-2">
-      {
-        data.map((bucket, i) => {
-          return (<div key={i} className="border border-grey-500 p-1 w-6/12 bg-blue-200 rounded-[1rem] mb-3 pt-2">
-            <div className="font-semibold text-black text-xl p-2">{bucket.name}</div>
+      {data.map((bucket, i) => {
+        return (
+          <div
+            key={i}
+            className="border-grey-500 mb-3 rounded-2xl border bg-blue-200 p-1 pt-2"
+          >
+            <div className="p-2 text-xl font-semibold text-black">
+              {bucket.name}
+            </div>
 
             {bucket.items.map((item, j) => {
-              return (<div key={j} className="border-2 border-b-0 border-gray-200 p-4 bg-white m-1 rounded">
-                <div className="row-2">
-                    <div className="grid grid-cols-6">
-                      <label className="font-bold text-xl text-center pt-3">
-                        <span>{item.legs[0].departure.substr(11, 5) + " "}</span>
-                        <span className="font-normal">{item.legs[0].origin.city}</span>
-                      </label>
- 
-                      <div className="text-gray-600 text-sm text-center pt-3">
-                        duration: {Math.floor(item.legs[0].durationInMinutes /60) + "h " + (((item.legs[0].durationInMinutes /60) - (Math.floor(item.legs[0].durationInMinutes /60)))*60).toFixed(0)}
-                        <div>
-                          {item.legs[0].stopCount ? "stop: " + item.legs[0].stopCount : "Direct"}
-                        </div>
-                      </div>
-                      <label className="font-bold text-xl text-center pt-3">
-                        <span>{item.legs[0].arrival.substr(11, 5) + " "}</span>
-                        <span className="font-normal">{item.legs[0].destination.city}</span>
-                      </label>
-                      <div className="text-center text-sm text-gray-500 pt-2 pl-1">
-                        {item.legs[0].carriers.marketing[0].name}
-                        <div className="flex justify-center">
-                          <img src={item.legs[0].carriers.marketing[0].logoUrl} className="w-6"/>
-                        </div>
-                      </div>
-                      <div className="font-bold text-sky-500 text-xl pl-12 col-end-6 pt-3">
-                        {item.price.formatted}
-                      </div>
-                      <a 
-                        className="bg-blue-800 rounded-[2rem] p-2 font-semibold text-gray-200 pl-7 text-xs" 
-                        href={item.deeplink} 
-                        target="_blank" 
-                        rel="noopener noreferrer">
-                        Find fights on SkyScanner
-                      </a>
-                    </div>
-                </div>
-                
-              </div>
-              );
-                
-            })}
+              return (
+                <div
+                  key={j}
+                  className="m-1 rounded border-2 border-b-0 border-gray-200 bg-white p-4"
+                >
+                  <div className="grid grid-cols-6">
+                    <label className="pt-3 text-center text-xl font-bold">
+                      <span>{item.legs[0].departure.substr(11, 5) + " "}</span>
+                      <span className="font-normal">
+                        {item.legs[0].origin.city}
+                      </span>
+                    </label>
 
-            
-          </div>)
-        })
-      }   
-     
+                    <div className="pt-3 text-center text-xs text-gray-600">
+                      Duration:{" "}
+                      {Math.floor(item.legs[0].durationInMinutes / 60) +
+                        "h " +
+                        (
+                          (item.legs[0].durationInMinutes / 60 -
+                            Math.floor(item.legs[0].durationInMinutes / 60)) *
+                          60
+                        ).toFixed(0)}
+                      <div className={item.legs[0].stopCount && "text-red-500"}>
+                        {item.legs[0].stopCount
+                          ? "Stops: " + item.legs[0].stopCount
+                          : "Direct"}
+                      </div>
+                    </div>
+                    <label className="pt-3 text-center text-xl font-bold">
+                      <span>{item.legs[0].arrival.substr(11, 5) + " "}</span>
+                      <span className="font-normal">
+                        {item.legs[0].destination.city}
+                      </span>
+                    </label>
+                    <div className="pl-1 text-center text-sm text-gray-500">
+                      <div className="mb-1 flex justify-center">
+                        <img
+                          src={item.legs[0].carriers.marketing[0].logoUrl}
+                          className="w-6"
+                        />
+                      </div>
+                      <div className="">
+                        {item.legs[0].carriers.marketing[0].name}
+                      </div>
+                    </div>
+                    <div className="col-end-6 pl-12 pt-3 text-xl font-bold text-sky-500">
+                      {item.price.formatted}
+                    </div>
+                    <a
+                      className="ml-auto w-24 rounded-full bg-blue-800 p-3 text-center font-semibold text-gray-200"
+                      href={item.deeplink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Book
+                    </a>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
